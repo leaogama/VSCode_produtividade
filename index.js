bfefab37ecec17bb83bf42f429a67117c5cd0a23
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2022-11-01 18:40:02 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+//fsdfsdf fdsfsd
+
+
+
+
+
+
+
+
+function helloworld (){
+console.log("Hello, World!!!")
+
+}
+
+const saudacao = () => {
+    var data = new Date()
+    return data.getHours() <=12? "Bom dia": data.getHours() <= 17? "Boa Tarde": "Boa Noite";
+}
+
+helloworld()
+console.log("A saudação do momento é " + saudacao());
