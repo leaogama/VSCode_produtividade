4801c2c161d21c45ff4981edb6104557a8f83bc7
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2022-11-01 18:51:34 -0300

    firacode

--- index.js
+++ index.js
@@ -14,14 +14,14 @@
 
 
 
-function helloworld (){
-console.log("Hello, World!!!")
+function helloworld() {
+    console.log("Hello, World!!!")
 
 }
 
 const saudacao = () => {
     var data = new Date()
-    return data.getHours() <=12? "Bom dia": data.getHours() <= 17? "Boa Tarde": "Boa Noite";
+    return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
 }
 
 helloworld()
