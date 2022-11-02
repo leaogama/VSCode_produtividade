700f4834af8737fcd18c2d21dc4eaaf2800017fb
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2022-11-01 22:44:05 -0300

    aula 10

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VSCode_produtividade
  Curso Produtividade com VSCode  - Wagner Aquino
+
+
+
+Consolas, 'Courier New', monospace => Fira Code
--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 
-
-
-
-
-
-
 //fsdfsdf fdsfsd
 
-
-
-
-
-
-
-
 function helloworld() {
-    console.log("Hello, World!!!")
-
+    //console.log("Hello, World!!!")
+    //console.log("Hello, World!!!")
+    //console.log("Hello, World!!!")
+    
 }
 
 const saudacao = () => {
-    var data = new Date()
-    return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
+    ////var data = new Date()
+    ////var data = new Date()
+    ////var data = new Date()
+    ////return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
+    ////return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
+    ////return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
 }
 
 helloworld()
 console.log("A saudação do momento é " + saudacao());
+
+console.log()
+console.log('dasdasd')
