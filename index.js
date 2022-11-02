3ba98cf98b32ac7d47d59dad31a85e70126eaec3
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2022-11-02 11:15:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const saudacao = () => {
     /var data = new Date()
     return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
     ////return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
-    ////return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 17 ? "Boa Tarde" : "Boa Noite";
+    
 }
 
 helloworld()
